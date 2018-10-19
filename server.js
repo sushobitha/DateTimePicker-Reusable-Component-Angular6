73a7a3aaa4d7bd9f6d4a967801683ae982936a70
sushobitha <sushobhithaps@gmail.com>
2018-10-20 02:47:53 +0530

    build fixes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Date-time'));
+app.use(express.static(__dirname + '/dist/DATE-TIME/Date-time'));
 
 app.get('*', (req, res) => {
-    res.sendFile(`./Date-time/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(`./DATE-TIME/Date-time/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 })
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
